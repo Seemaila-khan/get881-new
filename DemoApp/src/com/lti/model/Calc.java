96436d7434408a6a567a4d3ce36e1f6d6ec0496d
Seema <Seema@LAPTOP-1SM5FAE7>
2020-12-18 16:41:51 +0530

    added subs method from branch1

--- DemoApp/src/com/lti/model/Calc.java
+++ DemoApp/src/com/lti/model/Calc.java
@@ -4,4 +4,7 @@ public class Calc {
 	public void add(int n1, int n2) {
 		System.out.println("Summation=" + (n1 + n2));
 	}
+	public void subs(int n1, int n2) {
+		System.out.println("difference=" + (n1 - n2));
+	}
 }
